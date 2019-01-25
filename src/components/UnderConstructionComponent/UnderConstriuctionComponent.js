1a14e48eab06149539882b6959a30ac32cfd69c8
rajakumar <rajakumarofficial@gmail.com>
2019-01-26 00:08:32 +0530

    second commit

---
+++ src/components/UnderConstructionComponent/UnderConstriuctionComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class UnderConstructionComponent extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                <section class="UnderConstruction">
+                    <h1>Stay Tuned!</h1>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+
+export default UnderConstructionComponent
