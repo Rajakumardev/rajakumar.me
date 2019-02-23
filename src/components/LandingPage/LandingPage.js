be78b576044fa62a603230826b821a700b3d5cdb
rajakumar <rajakumarofficial@gmail.com>
2019-02-23 16:39:56 +0530

    landing-page||90%

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -8,7 +8,7 @@ class LandingPage extends Component {
         return (
             <Grid className="landing-grid">
                 <Cell col={12} className="hero-img-container">
-                    <img src={logo} className="hero-img"/>
+                    <img alt="rajakumar looking into the mirror" src={logo} className="hero-img"/>
                 </Cell>
                 <Cell col={12} className="desc-container">
                     <div style={{'font-family' :'Helvetica Neue','color':'white','width':'80%','margin':'0 auto'}}>
@@ -17,8 +17,11 @@ class LandingPage extends Component {
                         Had good exprience in tech industry. And also developed couple of projects️️️️ as an indie hacker.</h6>
                     </div>
                 </Cell>
-                <Cell col={12} className="Cell">
-
+                <Cell col={4} className="social-links">
+                    <h3><a href="https://twitter.com/srajakumar95"><i class="fab white fa-twitter"></i></a></h3>
+                    <h3><a href="https://www.linkedin.com/in/rajakumardev/"><i class="fab white fa-linkedin-in"></i></a></h3>
+                    <h3><a href="https://github.com/Rajakumardev"><i class="fab white fa-github-alt"></i></a></h3>
+                    <h3><a href="https://dev.to/rajakumardev"><i class="fab white fa-dev"></i></a></h3>
                 </Cell>
             </Grid>
         )
