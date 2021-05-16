cc5506916c72531be5b7a82500fd1ce7fdbacad0
Rajakumardev <rajakumarofficial@gmail.com>
2021-05-16 14:43:21 +0530

    gtag setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,26 +4,27 @@ module.exports = {
     description: "",
     siteUrl: "https://rajakumar.me"
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-sass",
-      options: {
-        implementation: require('sass')
-      }
+  plugins: [{
+    resolve: `gatsby-plugin-gtag`,
+    options: {
+      trackingId: `UA-184468386-1`,
+      head: false,
+      anonymize: true,
     },
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-184468386-1",
-      },
-    }, {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /assets/
-        }
+  },
+  {
+    resolve: "gatsby-plugin-sass",
+    options: {
+      implementation: require('sass')
+    }
+  }, {
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+      rule: {
+        include: /assets/
       }
-    },
+    }
+  },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
@@ -33,21 +34,21 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "images",
+      path: "./src/images/",
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
+    __key: "images",
+  },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "pages",
+      path: "./src/pages/",
     },
+    __key: "pages",
+  },
   ],
 };
