9f12fbfe7ca3dd15951c1d5ab0d28b66a9eb9b3b
Rajakumardev <rajakumarofficial@gmail.com>
2021-08-10 19:56:09 +0530

    seo component mig

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import Banner from '../components/Banner/Banner';
-import Grid from '../components/Grid/Grid';
 import Layout from '../components/layout/Layout';
+import Seo from '../components/Seo/Seo';
+
 function index() {
     return (
         <Layout>
-            <Helmet>
-                <html lang="en"/>
-                <meta name="google-site-verification" content="5txZwkAUwhgDgll3sWgdtziT1CMrXDejLOErZqCW65I" />
-                <title>rajakumar.me - developer writer</title>
-                <description>
-                    Hi, 👋 i have been creating web apps and android apps since 2010, 
-                    working as a 💻 engineer for 2+ years. 
-                    i created this blog to share my learning. 
-                    also you can join the newsletter to get updates.</description>
-            </Helmet>
+            <Seo title="Home"/>
             <Banner />
         </Layout>
     )
