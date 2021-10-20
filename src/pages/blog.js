627a144369b36bd5132a360bd63a1c5be3d6c1a8
Rajakumardev <rajakumarofficial@gmail.com>
2021-10-20 20:02:36 +0530

    grid spacing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,9 +4,10 @@ import { graphql, useStaticQuery } from 'gatsby'
 import Grid from '../components/Grid/Grid'
 import Layout from '../components/layout/Layout'
 import Seo from '../components/Seo/Seo'
+import Section from '../components/Section/Section'
 
 const Blog = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
         allMarkdownRemark {
           edges {
@@ -34,15 +35,17 @@ const Blog = () => {
       }
     `),
     postsEdge = data.allMarkdownRemark.edges || [];
-    console.log("data:",data);
-    return (
-        <Layout>
-          <Seo
-            title="Blog"
-          />
-            <Grid data={postsEdge}/>
-        </Layout>
-    )
+  console.log("data:", data);
+  return (
+    <Layout>
+      <Seo
+        title="Blog"
+      />
+      <Section className="section">
+        <Grid data={postsEdge} />
+      </Section>
+    </Layout>
+  )
 }
 
 export default Blog
