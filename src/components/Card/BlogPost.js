5904d97b1fadd0e3ee3b7e6a367a5e9fd2fc1db9
Rajakumardev <rajakumarofficial@gmail.com>
2021-10-20 21:09:42 +0530

    meta alignment

--- src/components/Card/BlogPost.js
+++ src/components/Card/BlogPost.js
@@ -3,18 +3,20 @@ import React from 'react'
 import Img from 'gatsby-image';
 
 const BlogPost = (props) => {
-    console.log("@@@:",props);
+    console.log("@@@:", props);
     return (
-            <Link to={`/blog/${props.link}`} className="item">
-                <div className="thumb">
-                    <Img fluid={props.featuredImage} />
-                </div>
-                <div className="details">
-                    <h4 className="title">{props.title}</h4>
+        <Link to={`/blog/${props.link}`} className="item">
+            <div className="thumb">
+                <Img fluid={props.featuredImage} />
+            </div>
+            <div className="details">
+                <h4 className="title">{props.title}</h4>
+                <div className="post-meta">
                     <p className="date">{props.date}</p>
                     <p className="readingTime">🕒 {props.readingTime}</p>
                 </div>
-            </Link>
+            </div>
+        </Link>
     )
 }
 
