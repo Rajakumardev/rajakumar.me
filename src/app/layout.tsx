b7823ce93056df9b5b2d5417450175a392a37c48
Rajakumardev <rajakumarofficial@gmail.com>
2023-09-21 09:38:44 +0530

    added progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Header, Navbar, NavbarItem } from '@/components';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Jost } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
 
 const jost = Jost({ subsets: ['latin'] });
 
@@ -19,6 +20,17 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${jost.className} bg-white dark:bg-green-950 px-8`}>
+				<NextTopLoader
+					color="#ffffff"
+					initialPosition={0.08}
+					crawlSpeed={200}
+					height={3}
+					crawl={true}
+					showSpinner={false}
+					easing="ease"
+					speed={200}
+					shadow="0 0 10px #ffffff,0 0 5px #ffffff"
+				/>
 				<Header icon="😸" title="RK">
 					<Navbar>
 						<NavbarItem text="Home" href="/" />
