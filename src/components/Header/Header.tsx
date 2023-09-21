d4781b0a7ab3affba7339f99648509e01fc700dc
Rajakumardev <rajakumarofficial@gmail.com>
2023-09-21 09:08:33 +0530

    header with dynamic aside component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 type Props = {
 	title: string;
 	icon: string;
+	children: JSX.Element[] | JSX.Element;
 };
 
-export const Header = ({ title = '', icon = '' }: Props) => {
+export const Header = ({ title = '', icon = '', children = [] }: Props) => {
 	return (
-		<div className="h-20 flex items-center">
-			<p
-				className=" text-2xl dark:border-white border-green-950 border-4 rounded-full
+		<div className="h-20 flex justify-between">
+			<div className="flex items-center w-28">
+				<p
+					className=" text-2xl dark:border-white border-green-950 border-4 rounded-full
             mr-4 p-1 border-dotted"
-			>
-				{icon}
-			</p>
-			<h1 className="dark:text-white text-2xl">{title}</h1>
+				>
+					{icon}
+				</p>
+				<h1 className="dark:text-white text-2xl">{title}</h1>
+			</div>
+			<div className="flex justify-end items-end">{children}</div>
 		</div>
 	);
 };
