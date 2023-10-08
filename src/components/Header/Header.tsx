dd68fe7773e85ff75b436c89c5a545df12c32562
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-08 17:28:14 +0530

    title changed to link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type Props = {
 	title: string;
 	icon: string;
@@ -7,7 +9,7 @@ type Props = {
 export const Header = ({ title = '', icon = '', children = [] }: Props) => {
 	return (
 		<div className="h-20 flex justify-between">
-			<div className="flex items-center w-28">
+			<Link href={'/'} className="flex items-center w-28">
 				<p
 					className=" text-2xl dark:border-white border-green-950 border-4 rounded-full
             mr-4 p-1 border-dotted"
@@ -15,7 +17,7 @@ export const Header = ({ title = '', icon = '', children = [] }: Props) => {
 					{icon}
 				</p>
 				<h1 className="dark:text-white text-2xl">{title}</h1>
-			</div>
+			</Link>
 			<div className="flex justify-end items-center">{children}</div>
 		</div>
 	);
