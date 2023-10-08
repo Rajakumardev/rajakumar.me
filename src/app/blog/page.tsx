fbd331004c24df1e247390fef901315ad52ed92a
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-08 18:20:40 +0530

    basic blog page and content sourcing - WIP

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,23 @@
+// import { PostCard } from '@/components';
+import { allPosts } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
 import React from 'react';
 
 const page = () => {
-	return <div>page</div>;
+	const posts = allPosts.sort((a, b) =>
+		compareDesc(new Date(a.date), new Date(b.date))
+	);
+
+	return (
+		<main className="flex flex-col items-center w-full scroll-smooth">
+			<h1 className="my-8 text-2xl dark:text-white text-green-950">
+				All Posts({posts.length})
+			</h1>
+			{posts.map((post, idx) => (
+				// <PostCard key={idx} {...post} />
+			))}
+		</main>
+	);
 };
 
 export default page;
