cd1e4d1d3f2566b8ea6e471640ae1ac623b8a74d
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-23 19:49:56 +0530

    responsive and other stylings

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -1,3 +1,4 @@
+import { textLimitter } from '@/utils';
 import { Post } from 'contentlayer/generated';
 import { format, parseISO } from 'date-fns';
 import Link from 'next/link';
@@ -5,15 +6,15 @@ import Link from 'next/link';
 export const PostCard = (post: Post) => {
 	return (
 		<Link href={post.url} className="w-1/2">
-			<div className="mb-8 border-2 border-dashed rounded-md p-4 flex gap-4">
+			<div className="mb-8 border-2 border-dashed rounded-md p-4 flex gap-4 flex-col lg:flex-row">
 				<img
 					src={post.featuredImage}
 					alt={post.title}
-					className="w-44 rounded-md"
+					className=" w-full lg:w-1/4 lg:h-full h-1/4 rounded-md object-cover"
 				/>
 				<div className="">
-					<h2 className="mb-1 text-3xl text-green-950 dark:text-white">
-						{post.title}
+					<h2 className="mb-1 text-2xl lg:text-3xl text-green-950 dark:text-white">
+						{textLimitter(post.title, 50)}
 					</h2>
 					<time
 						dateTime={post.date}
