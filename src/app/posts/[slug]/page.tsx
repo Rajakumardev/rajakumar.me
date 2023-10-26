6764f07625fad34d46945505df843ffd76016c44
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-26 09:15:33 +0530

    url case changes in the find check

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,14 +1,21 @@
 // app/posts/[slug]/page.tsx
 import { format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
+import { textLimitter } from '@/utils';
 
 export const generateStaticParams = async () =>
 	allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-	const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+	const post = allPosts.find(
+		(post) => post._raw.flattenedPath.toLowerCase() === params.slug
+	);
+	console.log('post:', post?.metaDescription);
 	if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-	return { title: post.title };
+	return {
+		title: post.title,
+		description: textLimitter(post.metaDescription, 160),
+	};
 };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
