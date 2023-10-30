0be9e3161d87372ed0ccf1fb3a5bcf8915051244
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-30 19:20:15 +0530

    grid alignment issue fixed

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -5,21 +5,18 @@ import Link from 'next/link';
 
 export const PostCard = (post: Post) => {
 	return (
-		<Link href={post.url} className="md:w-1/2 w-full">
-			<div className="mb-8 border-2 border-dashed rounded-md p-4 flex gap-4 flex-col lg:flex-row">
+		<Link href={post.url} className="md:w-[48%] w-full min-h-[10rem]">
+			<div className="mb-8 border-2 border-dashed rounded-md p-4 flex gap-4 flex-col lg:flex-row h-full">
 				<img
 					src={post.featuredImage}
 					alt={post.title}
-					className=" w-full lg:w-1/4 lg:h-full h-1/4 rounded-md object-cover"
+					className=" w-full lg:w-1/4 md:h-full h-1/4 rounded-md object-cover"
 				/>
 				<div className="">
 					<h2 className="mb-1 text-2xl lg:text-3xl text-white">
 						{textLimitter(post.title, 50)}
 					</h2>
-					<time
-						dateTime={post.date}
-						className="mb-2 block text-xs text-gray-300"
-					>
+					<time dateTime={post.date} className="mb-2 text-xs text-gray-300">
 						{format(parseISO(post.date), 'LLLL d, yyyy')}
 					</time>
 				</div>
