b66857f82c21297491d87f280f32482202d80565
Rajakumardev <rajakumarofficial@gmail.com>
2025-05-12 19:49:09 +0530

    added prjs links in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,25 @@ type Props = {
 
 export const Footer = ({ title = '' }: Props) => {
 	return (
-		<div className="h-20 flex justify-center items-center">
+		<div className="h-20 flex flex-col md:flex-row justify-center items-center gap-4">
 			<p className="text-white ">{title}</p>
+			<div className="flex flex-col md:flex-row items-center justify-center gap-4">
+				<h3 className="text-white">Other Projects :</h3>
+				<a
+					href="https://testmic.app"
+					target="_blank"
+					className="text-white underline"
+				>
+					Testmic.app
+				</a>
+				<a
+					href="https://penandpoetry.com"
+					target="_blank"
+					className="text-white underline"
+				>
+					Pen and Poetry Blog
+				</a>
+			</div>
 		</div>
 	);
 };
